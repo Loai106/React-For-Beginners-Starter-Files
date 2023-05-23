ce8d6fa51916f336aa994a8883ac1303aa8c0ac3
Loai <43700105+Loai106@users.noreply.github.com>
2023-05-24 00:30:38 +0300

    uploading the changes

---
+++ catch-of-the-day/src/components/Header.js
@@ -0,0 +1,16 @@
+import React  from "react";
+
+class Header extends React.Component{
+    render(){
+        return(
+            <Header className="top">
+                <h1>cathc of the day</h1>
+                <h3 className="tagline">
+                    <span>Fresh SeaFood Market</span>
+                </h3>
+            </Header>
+            );
+    }
+}
+
+export default Header;  
